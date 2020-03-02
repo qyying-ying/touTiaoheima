bfc250b5b2e87092b6a2b6210fa6cef984f0f553
jessie <1143994606@qq.com>
2020-03-02 19:47:37 +0800

    安装axios-配置全局使用

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,23 +7,23 @@
       <div class="title">
           <img src="../../assets/img/logo_index.png" alt="">
       </div>
-      <!-- 表单 -->
-      <el-form>
-          <!-- 表单容器 -->
-          <el-form-item style="margin-top:20px">
+      <!-- 表单 绑定model属性 绑定rules属性（表单验证规则）-->
+      <el-form :model="loginForm" :rules="loginRules" style="margin-top:20px">
+          <!-- 表单容器 prop表示要检验的字段名-->
+          <el-form-item prop="mobile">
               <!-- 表单域 -->
-              <el-input placeholder="请输入手机号"></el-input>
+              <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
           </el-form-item>
           <!-- 验证码 -->
-          <el-form-item>
-              <el-input style="width:60%" placeholder="请输入验证码"></el-input>
+          <el-form-item prop="code">
+              <el-input v-model="loginForm.code" style="width:60%" placeholder="请输入验证码"></el-input>
           <!-- 放置按钮 -->
           <el-button style="float:right" plain >发送验证码</el-button>
           </el-form-item>
           <!-- 表单域 -->
-          <el-form-item>
+          <el-form-item prop="checked">
               <!-- 是否同意 -->
-              <el-checkbox>我已阅读同意用户协议和隐私条款</el-checkbox>
+              <el-checkbox v-model="loginForm.checked">我已阅读同意用户协议和隐私条款</el-checkbox>
           </el-form-item>
           <!-- 按钮 -->
           <el-form-item>
@@ -36,8 +36,40 @@
 </template>
 
 <script>
+// 不知道从哪里来的 突然出先 待观察是否有用 import func from '../../../vue-temp/vue-editor-bridge'
 export default {
-
+  data () {
+    return {
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        checked: false // 是否同意用户
+      },
+      // 定义表单的验证规则
+      loginRules: {
+        //  required为true 表示该字段不能为空
+        mobile: [{ required: true, message: '你的手机号不能为空' }, {
+          pattern: /^1[3-9]\d{9}$/,
+          message: '您的手机号格式不正确'
+        }],
+        code: [{ required: true, message: '你的验证码不能为空' }, {
+          pattern: /^\d{6}$/,
+          message: '验证码应该为6位数字'
+        }],
+        checked: [{
+          validator: function (rule, value, callback) {
+            // rule是当前的校验规则
+            // value是当前的要校验的字段的值
+            // callback是一个回调函数 不论成功或者失败都要执行
+            // 成功执行callback 失败执行callback（new Error（错误信息））
+            // 我们认为 如果 value 为true就表示 校验成功 如果value为false 就表示校验失败
+            // new Error（错误信息）就是我们提示的错误信息
+            value ? callback() : callback(new Error('必须同意哦 不然不爱你啦'))
+          }
+        }]
+      }
+    }
+  }
 }
 </script>
 
